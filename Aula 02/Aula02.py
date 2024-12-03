aa8a8e2b6ac7f2a1607bf3366178f005696b2286
Camun05 <andrade.thayssa05@gmail.com>
2024-12-03 11:32:43 -0300

    Add files via upload
    aula 2 - Variáveis
    DATA: 03/12/24

---
+++ Aula 02/Aula02.py
@@ -0,0 +1,11 @@
+print('com grandes poderes vem grandes responsabilidades')
+
+nome_usuario = 'thayssa' # tipo -> srt = string texto
+idade_usuario = 20  # tipo -> int = integer número sem aspas
+altura_usuario = 1.58 # tipo -> float = ponto flutuante (casa decimal com .)
+maior_idade = True # tio -> bool = boolean (verdadeiro ou falso)
+
+print('Seja bem-vindo(a)!', nome_usuario)
+print ('Sua idade é', idade_usuario, 'anos.')
+print ('Você tem', altura_usuario, 'metros.')
+print('Você é maior de idade?', maior_idade)
